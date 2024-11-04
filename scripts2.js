d757cf705483cff8ecfa1cf263c0993a1a9fb966
Davi <davi.martins1362@gmail.com>
2024-11-04 10:36:46 -0300

    versão oficial

---
+++ scripts2.js
@@ -0,0 +1,29 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      0:{
+        slidesPerView: 1
+      },
+      768:{
+        slidesPerView: 2
+      },
+      1024:{
+        slidesPerView: 3
+      },
+    }
+  });
